e838130fc9b11160e73036717ceb96fffe78ed1e
Hamster <hamnoemireyeshrdz@gmail.com>
2025-08-21 20:20:09 +0000

    class 8

---
+++ if-exercises.py
@@ -0,0 +1,11 @@
+#Eassy
+def main():
+    b = int(input("Write a number: "))
+    print("Your absolute value is: ",number_e(b)*-1)
+    number_e(nou)
+
+def number_e(no):
+   if n:
+    < 0 b * b -1
+
+main()
