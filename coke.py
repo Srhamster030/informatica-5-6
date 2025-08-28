2e0d56899f5c6d430ce3121bd72b1012bd03a64a
Hamster <hamnoemireyeshrdz@gmail.com>
2025-08-28 17:30:22 +0000

    class 11

---
+++ coke.py
@@ -0,0 +1,9 @@
+def main():
+    b = 25, 10, 5
+    b = int(input("Insert a 25, 10 or 5 cents: "))
+    print("You have insert: ",b)
+    
+    n = input("Write your name: ")
+    print("Your name is: ",n)
+    
+main()
---
+++ jumps.py
@@ -0,0 +1,7 @@
+def main():
+    b = int(input("Write a number: "))
+    while True: 
+        if b == 5: b < 5
+        break
+    
+main()
