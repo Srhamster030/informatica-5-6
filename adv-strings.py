ea9cf8c69060a8e435a5b69e60957a4d5e3ecf08
Hamster <hamnoemireyeshrdz@gmail.com>
2025-09-02 16:40:03 +0000

    class 14

---
+++ adv-strings.py
@@ -0,0 +1,24 @@
+#String Lenght
+input_string = input("Please write a message: ")
+# print (input_string)
+
+# print("-" * len(input_string))
+
+# #String Index
+# print(input_string[0])
+# print(input_string[1])
+# print(input_string[-1])
+
+#Example
+i = 0
+# while i < len(input_string):
+#     print(input_string[i])
+#     i -= 1
+
+print("b" in input_string) 
+print("n" in input_string) 
+print("asa" in input_string) 
+
+
+
+
