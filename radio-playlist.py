557b9666cd749541c54b8ea28ad67031ab33a0c5
Hamster <hamnoemireyeshrdz@gmail.com>
2025-09-10 20:20:25 +0000

    class 17

---
+++ radio-playlist.py
@@ -0,0 +1,12 @@
+weekly_playlist = ["Blinding Lights", "Levitating", "As It Was", "Heat Waves", "Good 4 u"]
+print(weekly_playlist[1])
+weekly_playlist.append("drivers license")#add it to the playlist.
+weekly_playlist.insert(0,"Bohemian Rapsody")#add at the beginning of the playlist.
+weekly_playlist.remove("Good 4 u")#Take it out of the playlist.
+print(weekly_playlist.index("Levitating"))
+print("songs by Harry Styles: ", "as it was")
+
+
+
+
+print(weekly_playlist)
