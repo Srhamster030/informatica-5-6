210993fbf5899bb8a0112904f70fc82fac997754
Hamster <hamnoemireyeshrdz@gmail.com>
2025-09-17 20:21:36 +0000

    class 20

---
+++ for_intro.py
@@ -0,0 +1,13 @@
+doctrine =["Faith", "Repentance", "Baptism", "Holy Ghost", "Endure to the end"]
+
+# i = 0
+# while i < len(doctrine):
+#     print(doctrine[i])
+#     i+= 1
+
+for principle in doctrine:
+    print(principle)
+
+school_objective = ["Deepen conversion into Jesus Christ and Hs restored Gospel",  "Qualify for the blessings of the Temple", "Prepare for missionary and Church service", "Build the Kingdom of God"]
+print(objective)
+print("🔆")
