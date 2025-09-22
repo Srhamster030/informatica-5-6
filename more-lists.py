ea2840bbca0f5e8c5b3179f41007a2dcb093615d
Hamster <hamnoemireyeshrdz@gmail.com>
2025-09-22 19:40:01 +0000

    class 23

---
+++ more-lists.py
@@ -0,0 +1,25 @@
+# names = ["Bob", "Alex", "Kevin"]
+# names.append("Joseph")
+
+# for name in sorted(names):
+#     print(names)
+
+# measurements = [-2.5, 1.1, 7.5, 14.6, 21.0, 19.2] / len(measurements)
+# print("The mean is:", mean)
+
+#List within lists
+# super_lists = [[5,2,3], [4,2],[2,2,5,1]]
+# print(super_lists[1][0])
+
+# grades = [["Papu", 8, "S"], ["Melissa", 0,"D"], ["Shensi", 10, "L"], ["Tilín", 9, "G"], [":V", 10, ":V"]]
+# for student in grades:
+#     name = student[0]
+#     grade = student[1]
+#     group = student[2]
+#     print(f"{name} from group {group} got a {grade}")
+
+#Matrices :v
+matrix = [[1,2,3], [4,5,6], [7,8,9]]
+#print rows
+for row in matrix:
+    print(row)
