236bb92290e05e34b8a07ee4b59b18183f89262b
Hamster <hamnoemireyeshrdz@gmail.com>
2025-09-24 20:20:31 +0000

    class 25

--- matrix-operations.py
+++ matrix-operations.py
@@ -1,4 +1,4 @@
-def sum_of_row(matrix, row_number: int):
+def sum_of_row(matrix, row_number: int): #defining what do we want to do 
     row = matrix[row_number]
     row_sum = 0
     for item in row:
@@ -7,12 +7,20 @@ def sum_of_row(matrix, row_number: int):
 
     return row_sum
 
-def sum_of_colum(matrix, colum_number: int):
+def sum_of_colum(matrix, column_number: int): #we expect a matrix an we expect an interger.
     column_sum = 0
-    for row in matrix:
-        colum_sum = colum_sum + row[colum_number]
+    for row in matrix: #for every row
+        column_sum = column_sum + row[column_number] # add to the row in index x
     return column_sum
 
+def change_value(matrix, row_number, column_number, new_value):
+    row = matrix([row_number])
+    row[column_number] = new_value 
 m = [[4, 2, 3, 2], [9, 12, 11], [7, 8, 9, 5], [2, 9, 15, 1]]
-my_sum = sum_of_row(m, 2)
-print(my_sum)
+#my_sum = sum_of_row(m, 1)
+# my_sum = sum_of_row(m, 2)
+# print(m[2][3]) // "row 2, column 3"
+
+print(m)
+change_value(m,2,3,1000)
+print(m)
