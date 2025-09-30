f2b3e2e87274d75ead290de17e5e71a247e52308
Hamster <hamnoemireyeshrdz@gmail.com>
2025-09-30 16:40:13 +0000

    class 29

---
+++ character-count.py
@@ -0,0 +1,14 @@
+def character_counter(message, dictionary):
+    for character in message:
+        dictionary.setdefault(character, 0) #Si la letra está dos veces, la ignora y solo pone 1 en lugar de 0 y 2 en lugar de 1. 
+        dictionary[character] += 1
+
+    print(dictionary)
+    
+message = input("Write a message: ")
+dictionary = {}
+character_counter(message, dictionary)
+
+def mean_character(message, dictionary):
+    for mean in message:
+        
