721b72a35b9d3d66c543ab05c4de2a241c99e1f0
Hamster <hamnoemireyeshrdz@gmail.com>
2025-10-02 15:44:39 +0000

    class 31

--- character-count.py
+++ character-count.py
@@ -4,10 +4,21 @@ def character_counter(message, dictionary):
         dictionary[character] += 1
 
     print(dictionary)
+    print(sum(dictionary.values()))
     
+    #Alternative 1
+    values_list = list(dictionary.values())
+    # print(values_list)
+    largest_number_index = values_list.index(max(values_list)) #// we put .index to know in what number is the letra.// to return index
+    repeated_character = list(dictionary.keys())[largest_number_index]
+    print(f"The most repeated character is: {repeated_character}, repeating{dictionary[repeated_character]} times.")
+
 message = input("Write a message: ")
 dictionary = {}
 character_counter(message, dictionary)
 
-print(f"The most repeated number is:{len(message)}")
-        
+# character_counter(message, dictionary)
+
+# print(f"The most repeated number is:{len(message)}")
+
+#greater
