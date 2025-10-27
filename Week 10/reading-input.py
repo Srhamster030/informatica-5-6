a7195cbd847a366091aac047c989586899fc91a0
Hamster <hamnoemireyeshrdz@gmail.com>
2025-10-27 19:40:33 +0000

    class 35

---
+++ Week 10/reading-input.py
@@ -0,0 +1,19 @@
+def main():
+  number = read_input("Please type in a number: ", 5, 10)  #Define the funcions with 3 parameters
+  print("You typed in:", number)
+
+def read_input(prompt, small, large): # Insert missing parameters
+  try:
+    x = int(input(prompt))
+    if 5 < x < 10:
+      return x
+    else:
+      print("Please type a number BETWEEN" , small, "and", large)
+  except ValueError:
+    print("Please type a number between", small, "and", large)
+
+  
+  
+  # Complete the program here
+
+main()
