3200d934cebfe4b18b9beff3f96a4ee66a2d43ce
Hamster <hamnoemireyeshrdz@gmail.com>
2025-10-30 17:30:07 +0000

    class 36

---
+++ Week 10/factorial.py
@@ -0,0 +1,25 @@
+# def divide(a,b):
+#     if b == 0:
+#         raise ValueError("Hey! You cannot divide by zero.")
+#     return a / b
+
+# print(divide(1,2))
+# print(divide(2,0))
+
+def main():
+    x = (int(input("Type a positive integer: ")))
+    print(f"{x}! = {factorial(x)}")
+
+def factorial():
+        
+        if x > 1:
+            print(f"ÑAM")
+            break
+        except ValueError:
+
+        # else:
+        #  print("Type a positive number:")
+        
+
+
+main()
--- Week 10/reading-input.py
+++ Week 10/reading-input.py
@@ -3,17 +3,14 @@ def main():
   print("You typed in:", number)
 
 def read_input(prompt, small, large): # Insert missing parameters
-  try:
-    x = int(input(prompt))
-    if 5 < x < 10:
-      return x
-    else:
-      print("Please type a number BETWEEN" , small, "and", large)
-  except ValueError:
-    print("Please type a number between", small, "and", large)
-
-  
-  
-  # Complete the program here
+  while True:   
+    try:
+      x = int(input(prompt))
+      if 5 < x < 10:
+        return x
+      else:
+        print("Please type a number BETWEEN" , small, "and", large)
+    except ValueError:
+      print("Please type a number between", small, "and", large)
 
 main()
