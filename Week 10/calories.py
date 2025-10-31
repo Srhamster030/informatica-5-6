8bbbb831aef1f1ba82e68c7d2097598b124121e3
Hamster <hamnoemireyeshrdz@gmail.com>
2025-10-31 17:30:27 +0000

    class 37

---
+++ Week 10/calories.py
@@ -0,0 +1,19 @@
+def main():
+    food = ["whole milk", 
+            "watermelon", 
+            "corn:", "2 cal"
+            "potatoes:", "3 cal"
+            
+    ]
+    
+    while True:
+        food1 = input("Type the name of a food : ")
+        food2 = input("Type the name of another food : ")
+        input("press enter")
+        if (food1, food2):food
+        print(f"Number of calories of the two foods combined: ", {food1 + food2})
+        break
+
+    else:
+        print("NO")
+main()
