a2feb66f11cca38f47a9f1341734b73c0ce67099
Hamster <hamnoemireyeshrdz@gmail.com>
2025-11-04 16:31:42 +0000

    class 38

---
+++ week 11/libraries.py
@@ -0,0 +1,25 @@
+# import random
+
+# coin = random.choice(["heads", "tails"])
+# print(coin)
+
+# number = random.randint(1,10)
+# print(number)
+
+# cards = ["jack", "queen", "king", "Ace"]
+# random.shuffle(cards)
+# for card in cards:
+#     print(cards)
+
+# import statistics
+# print(statistics.mean([100, 90]))
+
+import statics
+import sys 
+print(statics.mean("Hello, my name is", sys.argv[1], sys.argv[[2]]))
+
+import sysi,port cowsay
+try:
+    cowsay.cow("hELLO, MY NAME IS", SYS.ARGV<[1]
+EXCEPT IndexError:
+#print("Too few arguments")
