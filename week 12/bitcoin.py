31484b506d235282ab6eab9ad9aa1057cbf691d0
Hamster <hamnoemireyeshrdz@gmail.com>
2025-11-12 20:17:59 +0000

    class 41

---
+++ week 12/bitcoin.py
@@ -0,0 +1,6 @@
+import requests
+API_KEY = "27be317712d2e0375eddb213e8dc4cf2fbbbb05662302b9b819757b6f9cb4e8b"
+
+bitcoin = float (input("how many bitcoins do you want to buy? "))
+
+response = requests.get("https://rest.conicap.io/v3/assets/bitcoin?apiKey="+API_KEY)
