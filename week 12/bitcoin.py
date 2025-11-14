f6cc8c6bbfd970f65dd51542f0d6b50eec5e9f53
Hamster <hamnoemireyeshrdz@gmail.com>
2025-11-14 21:57:43 +0000

    class 42

--- week 12/bitcoin.py
+++ week 12/bitcoin.py
@@ -1,6 +1,11 @@
 import requests
 API_KEY = "27be317712d2e0375eddb213e8dc4cf2fbbbb05662302b9b819757b6f9cb4e8b"
 
-bitcoin = float (input("how many bitcoins do you want to buy? "))
+bitcoin = float(input("how many bitcoins do you want to buy? "))
 
-response = requests.get("https://rest.conicap.io/v3/assets/bitcoin?apiKey="+API_KEY)
+response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey="+API_KEY)
+bitcoin_price = float(response.json()["data"]["priceUsd"])
+print(bitcoin_price)
+
+total = round(bitcoin * bitcoin_price, 2)
+print(f"Your total will be: ${total:,.2f}")
